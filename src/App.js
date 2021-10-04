665a3ff0de3197d850be3cde841ae298dad86e70
CGstyling <christinaguseva@gmail.com>
2021-10-04 14:18:16 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const [color, setColor] = React.useState("Onbekend");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div>
+        <h1>State uitgelegd!</h1>
+        <h3>De gekozen kleur is: {color}</h3>
+
+        <button type="button" className="purple" onClick={ () => setColor("Paars")}>
+          paars
+        </button>
+
+        <button type="button" className="red" onClick={ () => setColor("Rood")}>
+          rood
+        </button>
+
+        <button type="button" className="pink" onClick={ () => setColor("Roze")}>
+          roze
+        </button>
+      </div>
+
   );
 }
 
